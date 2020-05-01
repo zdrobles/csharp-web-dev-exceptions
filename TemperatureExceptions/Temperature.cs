9412ea83775672aa0164f7742fcc949bdc0ebce1
Carly Langlois <cj.langlois.cl@gmail.com>
2020-05-01 17:57:46 -0500

    add temp class

---
+++ TemperatureExceptions/Temperature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace TemperatureExceptions
+{
+    public class Temperature
+    {
+
+        private double fahrenheit;
+        public static double AbsoluteZeroFahrenheit { get; } = -459.67;
+
+        public Temperature(double fahrenheit)
+        {
+            try
+            {
+                Fahrenheit = fahrenheit;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Fahrenheit = -459.67;
+            }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return fahrenheit;
+            }
+            set
+            {
+
+                if (value < AbsoluteZeroFahrenheit)
+                {
+                    throw new ArgumentOutOfRangeException("Value is below absolute zero");
+                }
+
+                fahrenheit = value;
+            }
+        }
+
+        /* rest of the class... */
+    }
+}
