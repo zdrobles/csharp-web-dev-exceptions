ee661280fd070d88b9b05e974dc1ca989ae997d2
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-05-02 13:49:49 -0400

    Completed exercise

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -1,27 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 
-//Exercises for the Exceptions Chapter
-
-namespace csharp_web_dev_lsn9exceptions
+namespace csharp_web_dev_exceptions
 {
-    class Program
+    internal class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName", "Student did not submit any work!");
+            }
+            else
+            {
+                if (fileName[^3..] == ".cs")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
         }
-        */
+
+
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            double num1 = 100;
+            double num2 = 0;
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +59,22 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> kvp in students)
+            {
+                int points = 0;
+                try
+                {
+                    points = CheckFileExtension(kvp.Value);
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.Write(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine($"{kvp.Key} got {points} point{(points == 0 ? "s" : "")}.");
+                }
+            }
         }
     }
 }
